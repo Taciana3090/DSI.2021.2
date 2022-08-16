7ff6ef816ac807d6cdb0a89168f1ae44bd2f6087
Taciana3090 <taciana3090@gmail.com>
2022-08-16 09:44:04 -0300

    edição, named routes e classe de repositório

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,78 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
+class Words {
+  final _suggestions = <Word>[];
+  final _favorites = <Word>{};
+  bool _view = true;
+
+  Words() {
+    generateSuggestion(20);
+  }
+
+  void generateSuggestion(int count) {
+    for (var i = 0; i < count; i++) {
+      _suggestions.add(Word());
+    }
+  }
+
+  List getAllSuggestions() {
+    return _suggestions;
+  }
+
+  Word getSuggestionByIndex(int index) {
+    return _suggestions[index];
+  }
+
+  void removeSuggestion(Word value) {
+    _suggestions.remove(value);
+  }
+
+  void addToFavorites(Word value) {
+    _favorites.add(value);
+  }
+
+  void removeFavorite(Word value) {
+    _favorites.remove(value);
+  }
+
+  Set getAllFavorites() {
+    return _favorites;
+  }
+
+  bool isFavorite(Word value) {
+    return _favorites.contains(value);
+  }
+
+  bool view() {
+    return _view;
+  }
+
+  void alterView(bool value) {
+    _view = value;
+  }
+}
+
+class Word {
+  String first = '';
+  String second = '';
+
+  Word() {
+    var word = generateWordPairs().first;
+    first = word.first;
+    second = word.second;
+  }
+
+  String asPascalCase() {
+    return first[0].toUpperCase() +
+        first.substring(1) +
+        second[0].toUpperCase() +
+        second.substring(1);
+  }
+}
+
+Words words = Words();
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,27 +86,28 @@ class MyApp extends StatelessWidget {
       title: 'Startup Name Generetor',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.orange,
-        ),
+            backgroundColor: Colors.white, foregroundColor: Colors.orange),
       ),
-      home: const RandomWords(),
+      initialRoute: '/',
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        EditScreen.routeName: (context) => const EditScreen(),
+        SaveScreen.routeName: (context) => const SaveScreen(),
+      },
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  static const routeName = '/';
 
   @override
-  State<RandomWords> createState() => _RandomWordsState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+class _HomeScreenState extends State<HomeScreen> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = <WordPair>{};
-  bool _viewList = true;
 
   @override
   Widget build(BuildContext context) {
@@ -43,18 +116,20 @@ class _RandomWordsState extends State<RandomWords> {
         title: const Text('Startup Name Generator'),
         actions: [
           IconButton(
-            onPressed: _pushsaved,
+            onPressed: () => Navigator.pushNamed(context, '/saved'),
             icon: const Icon(Icons.favorite),
             tooltip: 'Sugestões salvas',
           ),
         ],
         leading: IconButton(
           onPressed: () {
-            setState(() {
-              _viewList ? _viewList = false : _viewList = true;
-            });
+            setState(
+              () {
+                words.view() ? words.alterView(false) : words.alterView(true);
+              },
+            );
           },
-          icon: Icon(_viewList ? Icons.grid_view : Icons.list),
+          icon: Icon(words.view() ? Icons.grid_view : Icons.list),
           tooltip: 'alterar visualização',
         ),
       ),
@@ -62,44 +137,17 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
-  void _pushsaved() {
-    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
-      final tiles = _saved.map((pair) {
-        return ListTile(
-          title: Text(
-            pair.asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      });
-      final divided = tiles.isNotEmpty
-          ? ListTile.divideTiles(
-              context: context,
-              tiles: tiles,
-            ).toList()
-          : <Widget>[];
-
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Sugestões salvas'),
-        ),
-        body: ListView(children: divided),
-      );
-    }));
-  }
-
   Widget _buildSuggestions() {
-    if (_viewList) {
+    if (words.view()) {
       return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return const Divider();
-
           final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
+          if (index >= words.getAllSuggestions().length) {
+            words.generateSuggestion(10);
           }
-          return _buildRow(_suggestions[index]);
+          return _buildRow(words.getSuggestionByIndex(index));
         },
       );
     } else {
@@ -107,49 +155,148 @@ class _RandomWordsState extends State<RandomWords> {
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           childAspectRatio: 4,
           crossAxisCount: 2,
-          crossAxisSpacing: 6.0,
-          mainAxisSpacing: 6.0,
+          crossAxisSpacing: 5.0,
+          mainAxisSpacing: 5.0,
         ),
         itemBuilder: (context, i) {
-          if (i >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
+          if (i >= words.getAllSuggestions().length) {
+            words.generateSuggestion(10);
           }
-          return Card(child: _buildRow(_suggestions[i]));
+          return Card(child: _buildRow(words.getSuggestionByIndex(i)));
         },
       );
     }
   }
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
+  Widget _buildRow(Word pair) {
+    final alreadySaved = words.isFavorite(pair);
     return Dismissible(
       key: Key(pair.toString()),
       onDismissed: (direction) {
         setState(() {
-          _suggestions.remove(pair);
-          _saved.remove(pair);
+          words.removeSuggestion(pair);
+          words.removeFavorite(pair);
         });
       },
       child: ListTile(
         title: Text(
-          pair.asPascalCase,
+          pair.asPascalCase(),
           style: _biggerFont,
         ),
         trailing: IconButton(
           icon: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border),
           color: alreadySaved ? Colors.red : null,
-          tooltip: alreadySaved ? 'remover dos salvos' : 'Salvos',
+          tooltip: alreadySaved ? 'remover dos salvos' : 'Salvo',
           onPressed: () {
             setState(() {
               if (alreadySaved) {
-                _saved.remove(pair);
+                words.removeFavorite(pair);
               } else {
-                _saved.add(pair);
+                words.addToFavorites(pair);
               }
             });
           },
         ),
+        onTap: () => Navigator.pushNamed(context, '/edit', arguments: pair),
       ),
     );
+  }
+}
+
+class SaveScreen extends StatelessWidget {
+  const SaveScreen({Key? key}) : super(key: key);
+  static const routeName = '/saved';
+
+  @override
+  Widget build(BuildContext context) {
+    final tiles = words.getAllFavorites().map((pair) {
+      return ListTile(
+        title: Text(
+          pair.asPascalCase(),
+          style: const TextStyle(fontSize: 18.0),
+        ),
+      );
+    });
+    final divided = tiles.isNotEmpty
+        ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList()
+        : <Widget>[];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sugestões salvas'),
+      ),
+      body: ListView(children: divided),
+    );
+  }
+}
+
+class EditScreen extends StatefulWidget {
+  const EditScreen({Key? key}) : super(key: key);
+  static const routeName = '/edit';
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final _palavra = ModalRoute.of(context)!.settings.arguments as Word;
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Editar página'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) => _validarEntrada(value!),
+                initialValue: _palavra.first,
+                onSaved: (texto) => _palavra.first = texto.toString(),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) => _validarEntrada(value!),
+                initialValue: _palavra.second,
+                onSaved: (texto) => _palavra.second = texto.toString(),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("com sucesso")));
+                  Navigator.popAndPushNamed(context, '/');
+                }
+              },
+              child: const Text('Enviar'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  String? _validarEntrada(String value) {
+    if (value.isEmpty) {
+      return 'Informe um valor';
+    }
+    return null;
   }
 }
